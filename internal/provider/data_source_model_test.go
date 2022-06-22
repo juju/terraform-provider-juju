5639f8535b4a24d392ee2d69a3bddfdf7d84cf46
Jonathan Ruckwood <jon-ruckwood@users.noreply.github.com>
2022-06-22 11:42:47 +0100

    Use provider to manage model for test

--- internal/provider/data_source_model_test.go
+++ internal/provider/data_source_model_test.go
@@ -2,15 +2,10 @@ package provider
 
 import (
 	"fmt"
-	"os/exec"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/juju/juju/cmd/modelcmd"
-	"github.com/juju/juju/jujuclient"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
@@ -29,77 +24,16 @@ func TestAcc_DataSourceModel(t *testing.T) {
 				),
 			},
 		},
-		CheckDestroy: testAccDataSourceModelDestroy,
 	})
 }
 
 func testAccDataSourceModel(t *testing.T, modelName string) string {
-	// TODO: required until we can use a resource to create a model
-	addModel(t, modelName)
-
 	return fmt.Sprintf(`
+resource "juju_model" "model" {
+	name = %q
+}
+
 data "juju_model" "model" {
-  name = %q
+  name = juju_model.model.name
 }`, modelName)
-}
-
-// This function destroys the model created for this test
-func testAccDataSourceModelDestroy(s *terraform.State) error {
-
-	for _, rs := range s.RootModule().Resources {
-		err := destroyModel(rs.Primary.Attributes["name"])
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// addModel adds a model using the Juju command-line
-//
-// This function will be removed once we can support creating a
-// model resource.
-func addModel(t *testing.T, modelName string) {
-	store := modelcmd.QualifyingClientStore{
-		ClientStore: jujuclient.NewFileClientStore(),
-	}
-
-	controllerName, err := store.CurrentController()
-	if err != nil {
-		t.Logf("warning: %s", controllerName)
-		return
-	}
-
-	cmd := exec.Command("juju", "add-model", "--no-switch", modelName)
-
-	err = cmd.Run()
-	if err != nil {
-		t.Fatalf("error whilst creating model %s: %s", modelName, err)
-	}
-}
-
-// destroyModel destroys a model using the Juju command-line
-//
-// This function will be removed once we can support destroying a
-// model resource.
-func destroyModel(modelName string) error {
-	store := modelcmd.QualifyingClientStore{
-		ClientStore: jujuclient.NewFileClientStore(),
-	}
-
-	controllerName, err := store.CurrentController()
-	if err != nil {
-		return fmt.Errorf("warning: %s", controllerName)
-	}
-
-	cmd := exec.Command("juju", "destroy-model", "-y", modelName)
-
-	err = cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error whilst destroying model %s: %s", modelName, err)
-	}
-
-	return nil
 }
