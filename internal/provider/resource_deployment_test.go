ee4b7409b9899310e3bbd1e6a5dc0febabb141f7
Matthew Revell <matthew.revell@opencredo.com>
2022-07-07 15:59:35 +0100

    Add juju_deployment test for updates unit/revision

--- internal/provider/resource_deployment_test.go
+++ internal/provider/resource_deployment_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAcc_ResourceDeployment(t *testing.T) {
+func TestAcc_ResourceDeployment_Basic(t *testing.T) {
 	modelName := acctest.RandomWithPrefix("tf-test-deployment")
 
 	resource.Test(t, resource.TestCase{
@@ -17,7 +17,7 @@ func TestAcc_ResourceDeployment(t *testing.T) {
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceDeployment(modelName),
+				Config: testAccResourceDeploymentBasic(modelName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("juju_deployment.this", "model", modelName),
 					resource.TestCheckResourceAttr("juju_deployment.this", "charm.#", "1"),
@@ -33,7 +33,41 @@ func TestAcc_ResourceDeployment(t *testing.T) {
 	})
 }
 
-func testAccResourceDeployment(modelName string) string {
+func TestAcc_ResourceDeployment_Updates(t *testing.T) {
+	modelName := acctest.RandomWithPrefix("tf-test-deployment")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceDeploymentUpdates(modelName, 1, 19),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("juju_deployment.this", "model", modelName),
+					resource.TestCheckResourceAttr("juju_deployment.this", "charm.#", "1"),
+					resource.TestCheckResourceAttr("juju_deployment.this", "charm.0.name", "ubuntu"),
+					resource.TestCheckResourceAttr("juju_deployment.this", "units", "1"),
+					resource.TestCheckResourceAttr("juju_deployment.this", "charm.0.revision", "19"),
+				),
+			},
+			{
+				Config: testAccResourceDeploymentUpdates(modelName, 2, 19),
+				Check:  resource.TestCheckResourceAttr("juju_deployment.this", "units", "2"),
+			},
+			{
+				Config: testAccResourceDeploymentUpdates(modelName, 2, 20),
+				Check:  resource.TestCheckResourceAttr("juju_deployment.this", "charm.0.revision", "20"),
+			},
+			{
+				ImportStateVerify: true,
+				ImportState:       true,
+				ResourceName:      "juju_deployment.this",
+			},
+		},
+	})
+}
+
+func testAccResourceDeploymentBasic(modelName string) string {
 	return fmt.Sprintf(`
 resource "juju_model" "this" {
   name = %q
@@ -46,4 +80,21 @@ resource "juju_deployment" "this" {
   }
 }
 `, modelName)
+}
+
+func testAccResourceDeploymentUpdates(modelName string, units int, revision int) string {
+	return fmt.Sprintf(`
+resource "juju_model" "this" {
+  name = %q
+}
+
+resource "juju_deployment" "this" {
+  model = juju_model.this.name
+  units = %d
+  charm {
+    name     = "ubuntu"
+    revision = %d
+  }
+}
+`, modelName, units, revision)
 }
