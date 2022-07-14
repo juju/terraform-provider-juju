532a79b0e4265c0cbbda49905c4a131ec66f178e
Jonathan Ruckwood <jon-ruckwood@users.noreply.github.com>
2022-07-14 11:05:59 +0100

    Make provider and requirer explicit in example

--- examples/resources/juju_integration/import.sh
+++ examples/resources/juju_integration/import.sh
@@ -1,2 +1,2 @@
-# Integrations can be imported by using the format: model_name:app1:endpoint:app2:endpoint, for example:
-$ terraform import juju_integration.wordpress_db development:wordpress:db:percona-cluster:server
+# Integrations can be imported by using the format: model_name:provider_app_name:endpoint:requirer_app_name:endpoint, for example:
+$ terraform import juju_integration.wordpress_db development:percona-cluster:server:wordpress:db
