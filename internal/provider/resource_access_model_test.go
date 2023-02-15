42aa3caaa4b6de3f1ab3aafc2bbc1a808e7546be
Mariyan Dimitrov <mariyan.dimitrov@gmail.com>
2023-02-15 23:15:43 +0200

    tests(access-model): Cover basic usage

---
+++ internal/provider/resource_access_model_test.go
@@ -0,0 +1,50 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAcc_ResourceAccessModel_Basic(t *testing.T) {
+	userName := acctest.RandomWithPrefix("tfuser")
+	userPassword := acctest.RandomWithPrefix("tf-test-user")
+	modelName := "testing"
+	access := "write"
+
+	resourceName := "juju_access_model.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceAccessModel(t, userName, userPassword, modelName, access),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "access", access),
+					resource.TestCheckResourceAttr(resourceName, "model", modelName),
+					resource.TestCheckTypeSetElemAttr(resourceName, "users.*", userName),
+				),
+			},
+		},
+	})
+}
+
+func testAccResourceAccessModel(t *testing.T, userName, userPassword, modelName, access string) string {
+	return fmt.Sprintf(`
+resource "juju_user" "this" {
+  name = %q
+  password = %q
+}
+
+resource "juju_model" "this" {
+  name = %q
+}
+
+resource "juju_access_model" "test" {
+  access = %q
+  model = juju_model.this.name
+  users = [juju_user.this.name]
+}`, userName, userPassword, modelName, access)
+}
