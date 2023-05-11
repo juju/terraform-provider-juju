54dbdaa9428d97f012d6668326ebd9e81e6a2736
Juan Tirado <juan.tirado@canonical.com>
2023-05-11 11:56:12 +0200

    Fix wrong charm name in test.

--- internal/provider/resource_application_test.go
+++ internal/provider/resource_application_test.go
@@ -32,7 +32,7 @@ func TestAcc_ResourceApplication_Basic(t *testing.T) {
 					resource.TestCheckResourceAttr("juju_application.this", "model", modelName),
 					resource.TestCheckResourceAttr("juju_application.this", "name", appName),
 					resource.TestCheckResourceAttr("juju_application.this", "charm.#", "1"),
-					resource.TestCheckResourceAttr("juju_application.this", "charm.0.name", "ubuntu"),
+					resource.TestCheckResourceAttr("juju_application.this", "charm.0.name", "jameinel-ubuntu-lite"),
 					resource.TestCheckResourceAttr("juju_application.this", "trust", "true"),
 					resource.TestCheckResourceAttr("juju_application.this", "expose.#", "1"),
 					resource.TestCheckResourceAttr("juju_application.this", "principal", "true"),
