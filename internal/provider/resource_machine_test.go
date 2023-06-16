900777cf00d516281450795abd8f898e8e668594
Caner Derici <caner.derici@canonical.com>
2023-06-16 17:11:14 -0600

    Add an integration test for add-machine

--- internal/provider/resource_machine_test.go
+++ internal/provider/resource_machine_test.go
@@ -46,3 +46,53 @@ resource "juju_machine" "this" {
 }
 `, modelName)
 }
+
+func TestAcc_ResourceMachine_AddMachine(t *testing.T) {
+	if testingCloud != LXDCloudTesting {
+		t.Skip(t.Name() + " only runs with LXD")
+	}
+	if testAddMachineIP == "" {
+		t.Errorf("environment variable %v not setup or invalid for running test", TestMachineIPEnvKey)
+	}
+	if testSSHPubKeyPath == "" || testSSHPrivKeyPath == "" {
+		t.Errorf("expected environment variables for ssh keys to be set : %v, %v",
+			TestSSHPublicKeyFileEnvKey, TestSSHPrivateKeyFileEnvKey)
+	}
+	modelName := acctest.RandomWithPrefix("tf-test-machine-ssh-address")
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccResourceMachineAddMachine(modelName, testAddMachineIP, testSSHPubKeyPath, testSSHPrivKeyPath),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("juju_machine.this_machine", "model", modelName),
+					resource.TestCheckResourceAttr("juju_machine.this_machine", "name", "manually_provisioned_machine"),
+					resource.TestCheckResourceAttr("juju_machine.this_machine", "machine_id", "0"),
+				),
+			},
+			{
+				ImportStateVerify: true,
+				ImportState:       true,
+				ResourceName:      "juju_machine.this_machine",
+			},
+		},
+	})
+}
+
+func testAccResourceMachineAddMachine(modelName string, IP string, pubKeyPath string, privKeyPath string) string {
+	return fmt.Sprintf(`
+resource "juju_model" "this_model" {
+	name = %q
+}
+
+resource "juju_machine" "this_machine" {
+	name = "manually_provisioned_machine"
+	model = juju_model.this_model.name
+
+	ssh_address = "ubuntu@%v"
+    public_key_file = %q
+    private_key_file = %q
+}
+`, modelName, IP, pubKeyPath, privKeyPath)
+}
