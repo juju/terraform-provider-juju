526a8849bba1f0a9f82b41b887151128cf025dbd
Caner Derici <caner.derici@canonical.com>
2023-07-25 13:05:33 -0600

    Mux resource_access_model tests

--- internal/provider/resource_access_model_test.go
+++ internal/provider/resource_access_model_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
-func TestAcc_ResourceAccessModel_Basic(t *testing.T) {
+func TestAcc_ResourceAccessModel_sdk2_framework_migrate(t *testing.T) {
 	userName := acctest.RandomWithPrefix("tfuser")
 	userPassword := acctest.RandomWithPrefix("tf-test-user")
 	modelName := "testing"
@@ -18,11 +18,11 @@ func TestAcc_ResourceAccessModel_Basic(t *testing.T) {
 
 	resourceName := "juju_access_model.test"
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t) },
-		ProviderFactories: providerFactories,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: muxProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config:      testAccResourceAccessModel(t, userName, userPassword, modelName, accessFail),
+				Config:      testAccResourceAccessModel_sdk2_framework_migrate(t, userName, userPassword, modelName, accessFail),
 				ExpectError: regexp.MustCompile("Error running pre-apply refresh.*"),
 			},
 			{
@@ -32,7 +32,7 @@ func TestAcc_ResourceAccessModel_Basic(t *testing.T) {
 				SkipFunc: func() (bool, error) {
 					return testingCloud != LXDCloudTesting, nil
 				},
-				Config: testAccResourceAccessModel(t, userName, userPassword, modelName, access),
+				Config: testAccResourceAccessModel_sdk2_framework_migrate(t, userName, userPassword, modelName, access),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "access", access),
 					resource.TestCheckResourceAttr(resourceName, "model", modelName),
@@ -52,7 +52,78 @@ func TestAcc_ResourceAccessModel_Basic(t *testing.T) {
 	})
 }
 
-func testAccResourceAccessModel(t *testing.T, userName, userPassword, modelName, access string) string {
+func testAccResourceAccessModel_sdk2_framework_migrate(t *testing.T, userName, userPassword, modelName, access string) string {
+	return fmt.Sprintf(`
+provider oldjuju {}
+
+resource "juju_user" "this" {
+  provider = oldjuju
+  name = %q
+  password = %q
+}
+
+resource "juju_model" "this" {
+  provider = oldjuju
+  name = %q
+}
+
+resource "juju_access_model" "test" {
+  provider = oldjuju
+  access = %q
+  model = juju_model.this.name
+  users = [juju_user.this.name]
+}`, userName, userPassword, modelName, access)
+}
+
+func TestAcc_ResourceAccessModel_Stable(t *testing.T) {
+	userName := acctest.RandomWithPrefix("tfuser")
+	userPassword := acctest.RandomWithPrefix("tf-test-user")
+	modelName := "testing"
+	access := "write"
+	accessFail := "bogus"
+
+	resourceName := "juju_access_model.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() { testAccPreCheck(t) },
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"juju": {
+				VersionConstraint: "0.8.0",
+				Source:            "juju/juju",
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccResourceAccessModel_Stable(t, userName, userPassword, modelName, accessFail),
+				ExpectError: regexp.MustCompile("Error running pre-apply refresh.*"),
+			},
+			{
+				// (juanmanuel-tirado) For some reason beyond my understanding,
+				// this test fails no microk8s on GitHub. If passes in local
+				// environments with no additional configurations...
+				SkipFunc: func() (bool, error) {
+					return testingCloud != LXDCloudTesting, nil
+				},
+				Config: testAccResourceAccessModel_Stable(t, userName, userPassword, modelName, access),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "access", access),
+					resource.TestCheckResourceAttr(resourceName, "model", modelName),
+					resource.TestCheckTypeSetElemAttr(resourceName, "users.*", userName),
+				),
+			},
+			{
+				SkipFunc: func() (bool, error) {
+					return testingCloud != LXDCloudTesting, nil
+				},
+				ImportStateVerify: true,
+				ImportState:       true,
+				ImportStateId:     fmt.Sprintf("%s:%s:%s", modelName, access, userName),
+				ResourceName:      resourceName,
+			},
+		},
+	})
+}
+
+func testAccResourceAccessModel_Stable(t *testing.T, userName, userPassword, modelName, access string) string {
 	return fmt.Sprintf(`
 resource "juju_user" "this" {
   name = %q
