1bc1e60f8da2a95e4e0114ab2e8b911846ce7e84
gatici <gulsum.atici@canonical.com>
2024-07-08 12:32:49 +0300

    chore: resolve merge conflicts
    Resolving the merge conflicts with main branch as
    another merged PRs created some conflicts.
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- examples/resources/juju_application/resource.tf
+++ examples/resources/juju_application/resource.tf
@@ -10,43 +10,17 @@ resource "juju_application" "this" {
     series   = "trusty"
   }
 
+  resources = {
+    gosherve-image = "gatici/gosherve:1.0"
+  }
+
   units = 3
 
-<<<<<<< HEAD
   placement = "0,1,2"
 
   storage_directives = {
     files = "101M"
   }
-
-=======
->>>>>>> 17accba (Removing additional example from resource.tf)
-  config = {
-    external-hostname = "..."
-  }
-}
-
-<<<<<<< HEAD
-resource "juju_application" "custom_resources_example" {
-  name  = "custom-resource-example"
-=======
-resource "juju_application" "placement_example" {
-  name  = "placement-example"
->>>>>>> 17accba (Removing additional example from resource.tf)
-  model = juju_model.development.name
-  charm {
-    name     = "hello-kubecon"
-    channel  = "edge"
-    revision = 14
-    series   = "trusty"
-  }
-
-  resources = {
-    gosherve-image = "gatici/gosherve:1.0"
-  }
-
-  units     = 3
-  placement = "0,1,2"
 
   config = {
     external-hostname = "..."
