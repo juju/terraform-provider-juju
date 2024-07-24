bd7b47d267b42b0c87b1232aefa15034f0539c74
gatici <gulsum.atici@canonical.com>
2024-07-24 15:32:20 +0300

    feat(application): adding custom plan validator for ResourceKey
    ResourceKey schema is validated using a custom PlanValidator named
    StringIsResourceKeyValidator to to ensure that the string is an
    int or OCI image information as a URL.
    This will allow for failure during the planning phase if the
    values are not as expected.
    
    Signed-off-by: gatici <gulsum.atici@canonical.com>

---
+++ internal/provider/validator_resourcekey_test.go
@@ -0,0 +1,12 @@
+// Copyright 2024 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package provider_test
+
+import (
+	"testing"
+)
+
+func TestResourceKeyValidatorValid(t *testing.T) {
+
+}
