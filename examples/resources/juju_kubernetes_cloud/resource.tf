1b21c20d9ba917b6b1cd43abd35235aaded6bfda
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2024-10-04 20:48:47 +0300

    docs: add model to the example

--- examples/resources/juju_kubernetes_cloud/resource.tf
+++ examples/resources/juju_kubernetes_cloud/resource.tf
@@ -2,3 +2,11 @@ resource "juju_kubernetes_cloud" "my-k8s-cloud" {
   name              = "my-k8s-cloud"
   kubernetes_config = file("<path-to-my-kubennetes-cloud-config>.yaml")
 }
+
+resource "juju_model" "my-model" {
+  name = "my-model"
+  credential = juju_kubernetes_cloud.my-k8s-cloud.credential
+  cloud {
+    name   = juju_kubernetes_cloud.my-k8s-cloud.name
+  }
+}
