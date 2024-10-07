c4fa324075693cf07a1ac121f42c376fdedb0a47
Vitaly Antonenko <vitaly.antonenko@canonical.com>
2024-10-07 16:34:54 +0300

    test: add ACC test with model

--- internal/provider/resource_kubernetes_cloud_test.go
+++ internal/provider/resource_kubernetes_cloud_test.go
@@ -17,6 +17,7 @@ func TestAcc_ResourceKubernetesCloud(t *testing.T) {
 		t.Skip(t.Name() + " only runs with LXD")
 	}
 	cloudName := acctest.RandomWithPrefix("tf-test-k8scloud")
+	modelName := acctest.RandomWithPrefix("tf-test-model")
 	cloudConfig := os.Getenv("MICROK8S_CONFIG")
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -24,7 +25,7 @@ func TestAcc_ResourceKubernetesCloud(t *testing.T) {
 		ProtoV6ProviderFactories: frameworkProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceKubernetesCloudWithoutModel(cloudName, cloudConfig),
+				Config: testAccResourceKubernetesCloud(cloudName, modelName, cloudConfig),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("juju_kubernetes_cloud."+cloudName, "name", cloudName),
 				),
@@ -33,39 +34,27 @@ func TestAcc_ResourceKubernetesCloud(t *testing.T) {
 	})
 }
 
-//func testAccResourceKubernetesCloud(cloudName string, modelName string, config string) string {
-//	return internaltesting.GetStringFromTemplateWithData(
-//		"testAccResourceSecret",
-//		`
-//resource "juju_kubernetes_cloud" {{.CloudName}} {
-//  name = "{{.CloudName}}"
-//  kubernetes_config = {{.Config}}
-//}
-//
-//resource "juju_model" {{.ModelName}} {
-//  name = "{{.ModelName}}"
-//  credential = juju_kubernetes_cloud.{{.CloudName}}.credential
-//  cloud {
-//    name   = juju_kubernetes_cloud.{{.CloudName}}.name
-//  }
-//}
-//`, internaltesting.TemplateData{
-//			"CloudName": cloudName,
-//			"ModelName": modelName,
-//			"Config":    config,
-//		})
-//}
-
-func testAccResourceKubernetesCloudWithoutModel(cloudName string, config string) string {
+func testAccResourceKubernetesCloud(cloudName string, modelName string, config string) string {
 	return internaltesting.GetStringFromTemplateWithData(
 		"testAccResourceSecret",
 		`
-resource "juju_kubernetes_cloud" {{.CloudName}} {
-  name = "{{.CloudName}}"
-  kubernetes_config = {{.Config}}
+
+
+resource "juju_kubernetes_cloud" "tf-test-k8scloud" {
+ name = "{{.CloudName}}"
+ kubernetes_config = {{.Config}}
+}
+
+resource "juju_model" {{.ModelName}} {
+ name = "{{.ModelName}}"
+ credential = juju_kubernetes_cloud.tf-test-k8scloud.credential
+ cloud {
+   name   = juju_kubernetes_cloud.tf-test-k8scloud.name
+ }
 }
 `, internaltesting.TemplateData{
 			"CloudName": cloudName,
+			"ModelName": modelName,
 			"Config":    config,
 		})
 }
