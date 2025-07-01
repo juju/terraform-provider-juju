c769179509f35ad37492dd16c7f54c11f1e8e387
SimoneDutto <simone.dutto@canonical.com>
2025-07-01 15:43:29 +0200

    feat(retry-pkg): add retry pkg to retry operation on retriable errors
    the retry pkg is a wrapper around wait to expose more explicity a util function to retry operations
    when we encounter retriable errors.

---
+++ internal/retry/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the Apache License, Version 2.0, see LICENCE file for details.
+
+// Package retry provides utilities for retry on conditions.
+// It includes functions to retry for specific data conditions or errors,
+// with support for retrying operations and handling non-fatal errors.
+
+package retry
