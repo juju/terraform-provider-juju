26ac5ef787188061fe9efc4110b15fdeaba288b2
SimoneDutto <simone.dutto@canonical.com>
2025-08-22 14:28:42 +0200

    feat: support null value in config
    In this PR we make use of map[string]*string to support null values in
    the config and issue a `reset` upon. I've created some utils methods
    that will be re-used by application config.

---
+++ internal/provider/export_test.go
@@ -0,0 +1,8 @@
+// Copyright 2025 Canonical Ltd.
+// Licensed under the Apache License, Version 2.0, see LICENCE file for details.
+
+package provider
+
+var NewConfig = newConfig
+var NewConfigFromModelConfigAPI = newConfigFromModelConfigAPI
+var ComputeConfigDiff = computeConfigDiff
