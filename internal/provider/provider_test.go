7ecb17bcc7a6ed0c789968b8f0429ad7b193c4b9
Kian Parvin <kian.parvin@canonical.com>
2025-09-12 08:55:12 +0200

    test: update set skipFailedDeletion env test

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -208,6 +208,7 @@ func TestProviderAllowsEmptyCACert(t *testing.T) {
 
 func TestProviderSetWarnOnDeletionErrors(t *testing.T) {
 	jujuProvider := NewJujuProvider("dev", true)
+	t.Setenv(SkipFailedDeletionEnvKey, "false")
 	confResp := configureProvider(t, jujuProvider)
 	providerData, ok := confResp.ResourceData.(juju.ProviderData)
 	require.Truef(t, ok, "ResourceData, not of type juju ProviderData")
