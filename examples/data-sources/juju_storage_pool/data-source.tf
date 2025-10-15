6608be5745342278b699fa6da8e762899c93fdc6
ale8k <alexjkilroy@gmail.com>
2025-10-15 10:34:45 +0100

    docs: update example doc

--- examples/data-sources/juju_storage_pool/data-source.tf
+++ examples/data-sources/juju_storage_pool/data-source.tf
@@ -5,4 +5,7 @@ data "juju_model" "my_model" {
 data "juju_storage_pool" "my_storage_pool_data_source" {
   name       = "my_storage_pool"
   model_uuid = data.juju_model.my_model.uuid
+  # Or:
+  model_name  = data.juju_model.my_model.name
+  model_owner = data.juju_model.my_model.owner
 }
