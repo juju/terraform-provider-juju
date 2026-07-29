5fdc285492ee62ce07d6d77be332a2c80e21f09e
Luci Brănescu <luci.branescu@canonical.com>
2026-07-29 12:06:58 +0100

    fix: slop

--- juju-tf-refwriter/main_test.go
+++ juju-tf-refwriter/main_test.go
@@ -2,10 +2,6 @@
 // Licensed under the AGPLv3, see LICENCE file for details.
 
 package main
-// Copyright 2026 Canonical Ltd.
-// Licensed under the AGPLv3, see LICENCE file for details.
-
-package main
 
 import (
 	"bytes"
