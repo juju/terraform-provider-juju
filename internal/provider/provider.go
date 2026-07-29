d23edbcb4287ebba77501195e7ec418bde16e0f6
SimoneDutto <simonedutto8@gmail.com>
2026-07-29 16:32:30 +0200

    chore: update
    Co-authored-by: Luci Brănescu <luci@bran.me.uk>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -258,7 +258,7 @@ func (p *jujuProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp 
 				Optional:    true,
 			},
 			LazyAPICheck: schema.BoolAttribute{
-				Description: "If set to true, the provider will not connect to the controller during planning. " +
+				Description: "If set to true, the provider will not connect to the controller during planning for creation. " +
 					"This allows for planning the creation of resources without a live controller. However, the provider will still " +
 					"connect to the controller when planning for updates." +
 					"Note that the validators/checks (e.g. check some resources are only valid with JAAS) will not be run until apply time.",
